14ada9ed8e085d567b8d2e50cb31091f139748ef
alessio iafrate <a.iafrate@gmail.com>
2025-02-24 21:02:33 +0100

    Refactor HomePage and update UI elements
    - Cleaned up formatting in HomePage.xaml.
    - Added new methods for synchronous operations in HomePage.xaml.cs, including logging functionality.
    - Adjusted event listener formatting in index.html.
    - Updated button elements in index.html for consistent styling.
    - Changed button background color in app.css from dodgerblue to red.

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace MauiNet9.Pages;
 
 public partial class HomePage : ContentPage
@@ -5,7 +7,8 @@ public partial class HomePage : ContentPage
 	public HomePage()
 	{
 		InitializeComponent();
-	}
+        hybridWebView.SetInvokeJavaScriptTarget(this);
+    }
 
     private void OnSendMessageButtonClicked(object sender, EventArgs e)
     {
@@ -15,5 +18,38 @@ public partial class HomePage : ContentPage
     private async void OnHybridWebViewRawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
     {
         await DisplayAlert("Raw Message Received", e.Message, "OK");
+    }
+
+
+    public void DoSyncWork()
+    {
+        Debug.WriteLine("DoSyncWork");
+    }
+
+    public void DoSyncWorkParams(int i, string s)
+    {
+        Debug.WriteLine($"DoSyncWorkParams: {i}, {s}");
+    }
+
+    public string DoSyncWorkReturn()
+    {
+        Debug.WriteLine("DoSyncWorkReturn");
+        return "Hello from C#!";
+    }
+
+    public SyncReturn DoSyncWorkParamsReturn(int i, string s)
+    {
+        Debug.WriteLine($"DoSyncWorkParamReturn: {i}, {s}");
+        return new SyncReturn
+        {
+            Message = "Hello from C#!" + s,
+            Value = i
+        };
+    }
+
+    public class SyncReturn
+    {
+        public string? Message { get; set; }
+        public int Value { get; set; }
     }
 }
